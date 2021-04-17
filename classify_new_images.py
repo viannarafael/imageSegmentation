63616f6f5254792cf3ce515f137fc51e15fa13fc
viannarafael <rafaelvianna@id.uff.br>
2021-04-17 17:23:54 -0300

    Modification

--- classify_new_images.py
+++ classify_new_images.py
@@ -1,9 +1,11 @@
 # ------------------------------------------------------------------
 #  Code to perform image segmentation using machine learning algorithms
 #  Autor: Rafael Vianna
-#  Date: 2020 July 
+#  Date: 2020 July
 # -----------------------------------------------------------------
 
+
+import pickle
 import cv2
 import matplotlib.pyplot as plt
 from features import extract_feature
@@ -15,26 +17,25 @@ name = "gfrp"
 
 # Import original image
 # ------------------------------------------------------------------
-img = cv2.imread(name +'.tif')
+img = cv2.imread(name + '.tif')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Feature Extraction
 # ------------------------------------------------------------------
-start=time.time()
+start = time.time()
 X = extract_feature(img)
-print("Feature Extraction time = ",time.time()-start)
-  
+print("Feature Extraction time = ", time.time()-start)
+
 # Load Model
 # ------------------------------------------------------------------
-import pickle
 filename = name + '_RF_classifier'
-model = pickle.load(open(filename, 'rb')) # Read binary file
+model = pickle.load(open(filename, 'rb'))  # Read binary file
 
 # Classification
 # ------------------------------------------------------------------
-start=time.time()
+start = time.time()
 classification = model.predict(X)
-print("Segmentation time = ",time.time()-start)
+print("Segmentation time = ", time.time()-start)
 
 # Show image
 segmented_image = classification.reshape((img.shape))
@@ -42,4 +43,3 @@ plt.imshow(segmented_image)
 
 # Save into a file
 plt.imsave('segmented_'+name+'_RF.jpg', segmented_image)
-
